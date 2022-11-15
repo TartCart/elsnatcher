330c30f92c63a88d549ca26a9f69c8ff52620363
carlsonmke <67134507+carlsonmke@users.noreply.github.com>
2022-11-15 11:03:25 -0500

    Add files via upload

---
+++ elsnatcher.ps1
@@ -0,0 +1,118 @@
+﻿
+# v2.0 
+#This script creates a directory as the machines hostname in the root of C:\ and pulls necessary event and IIS logs for typical engagements
+
+Write-Host "This script must be ran as admin" -Foregroundcolor Yellow
+Write-Host "it will start the extraction process with the most standard event logs, do you want to begin?" -ForegroundColor Yellow
+
+do{
+    $ans = Read-Host '(Y/N)'
+    if($ans -eq 'N'){Write-Host "???" -ForegroundColor Red
+     return }
+}
+until($ans -eq 'Y')
+
+
+#Creating directory
+$h = hostname
+
+New-Item C:\$h -itemType Directory | Out-Null
+
+$d = "C:\$h"
+write-host "`n"
+Write-Host "Directory created at C:\$h" 
+
+#Moving event log files
+cd C:\Windows\System32\winevt\logs\
+Write-Host "Copying event logs ..."
+Copy-item Application.evtx -Destination $d
+Copy-item Security.evtx -Destination $d
+Copy-item System.evtx -Destination $d
+Copy-item "Windows PowerShell.evtx" -Destination $d
+Copy-item *Microsoft-Windows-PowerShell%4Admin.evtx -Destination $d
+Copy-item *Microsoft-Windows-PowerShell%4Operational.evtx -Destination $d
+Copy-item *Microsoft-Windows-RemoteDesktopServices-RdpCoreTS%4Operational.evtx -Destination $d
+Copy-item *Defender*.evtx -Destination $d
+Write-Host "Completed all necessary event logs" -ForegroundColor Green
+Write-Host "`n"
+
+#Ask user if machine is exchange server to pull IIS and exchange logs
+Write-Host "Is this an exchange server? Entering 'Y' will copy over IIS and exchange event logs (if enabled and available.)" -foregroundcolor yellow
+Write-Host "Enter 'N' to return and end the script" -foregroundcolor yellow
+
+do{
+    $ans = Read-Host '(Y/N)'
+    if($ans -eq 'N'){Write-Host "The copied logs are located in C:\$h" -ForegroundColor Green
+    Write-Host "Don't forget to remove all .ps1 files/evidence, set the remote execution policy back to remote-signed"
+     return }
+}
+until($ans -eq 'Y')
+
+write-host "`n"
+Write-Host "Copying over exchange event logs, this may take some time depending on file size ..."
+
+Copy-item *Exchange*.evtx  -Destination $d
+
+#get user input for amount of days back to pull IIS logs 
+Write-Host "How many days back of IIS logs do you want to pull? Typical is 31, over 2 months - the file size could become too thick" -ForegroundColor Yellow
+
+do {
+  write-host -nonewline "Enter a numeric value: "
+  $inputString = read-host
+  $ageoflogsinput = $inputString -as [Double]
+  $ok = $ageoflogsinput -ne $NULL
+  if ( -not $ok ) { write-host "You must enter a numeric value" }
+}
+until ( $ok )
+
+$ageoflogs = -$ageoflogsinput
+
+cd C:\inetpub\logs\LogFiles
+
+New-Item C:\$h\iislogs -ItemType Directory | Out-Null
+
+Write-Host "Copying over IIS logs, this may take some time depending on file size ... "
+Write-Host "`n"
+Write-Host "Now would be a good time to get the Exchange Server version, instructions below" -ForegroundColor Yellow
+Write-Host "Open a Microsoft Exchange Management Console as Admin and paste in the following command: 'Get-ExchangeServer | Format-List Name, Edition, AdminDisplayVersion'"
+
+
+#Copy over IIS logs based on age
+$iislogs = Get-ChildItem *.log -Path . -Recurse | ? {$_.LastWriteTime -gt (Get-Date).AddDays($ageoflogs)} |Copy-Item -Destination C:\$h\iislogs
+write-host "`n"
+Write-Host "Exchange and IIS logs completed" -ForegroundColor Green
+Write-Host "`n"
+
+#get and tell user accumulated log file size and ask if want to compress/zip 
+$size = “{0:N2}” -f ((Get-ChildItem -path $d -recurse | Measure-Object -property length -sum ).sum /1GB) + ” GB”
+
+Write-Host "The file size of the accumulated event/logs is $size, do you want to compress the log file?" -foregroundcolor yellow
+Write-Host "Zipping in powershell is only available in versions 5.0 and above, the current version is listed below." 
+$PSVersionTable
+
+Write-Host "Enter 'n' if this machine is too old and dusty, you'll have to zip manually"
+do{
+    $ans = Read-Host '(Y/N)'
+    if($ans -eq 'N'){Write-Host "The copied logs are located in C:\$h, have a swell day" -ForegroundColor Green
+     return }
+}
+until($ans -eq 'Y')
+
+$final = "C:\$h.zip"
+
+Compress-Archive -Path C:\$h -DestinationPath $final
+Write-Host "`n"
+write-host "Compression complete, zipped file is located here: '$final' do you want the OG file to be deleted?" -ForegroundColor Green
+
+do{
+    $ans = Read-Host '(Y/N)'
+    if($ans -eq 'N'){Write-Host "Don't forget to remove all .ps1 files/evidence, set the remote execution policy back to remote-signed" -ForegroundColor Green
+     return }
+}
+until($ans -eq 'Y')
+
+#delete OG folder once compression is completed
+Remove-Item $d -Recurse -Force
+CD C:\
+Write-Host "`n"
+Write-Host "SUCCESS, Don't forget to remove all .ps1 files/evidence, set the remote execution policy back to remote-signed" -ForegroundColor Green
